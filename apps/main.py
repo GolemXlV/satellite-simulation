dc5ebac6ab913747c7252cabc2fb474e3672463a
GolemXIV <shaper2010@gmail.com>
2020-01-20 00:26:08 +0400

    added multiselect satellite real values

--- apps/main.py
+++ apps/main.py
@@ -5,32 +5,25 @@ import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
 import plotly.graph_objs as go
+from pathlib import Path
 
 from app import app
 
 if 'DYNO' in os.environ:
     app_name = os.environ['DASH_APP_NAME']
 else:
-    app_name = 'dash-3dscatterplot'
+    app_name = 'satellite-prediction'
 
-df = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/2011_us_ag_exports.csv")
+df = pd.read_csv(Path('apps/data/train.csv'), index_col=['id'])
 
 layout = html.Div([
-    html.Div([html.H1("Satellite position")],
+    html.Div([html.H1("Real Satellite position")],
              style={'textAlign': "center", "padding-bottom": "10", "padding-top": "10"}),
     html.Div(
-        [html.Div(dcc.Dropdown(id="select-xaxis", options=[{'label': i.title(), 'value': i} for i in df.columns[3:]],
-                               value='beef', ), className="four columns",
-                  style={"display": "block", "margin-left": "auto",
-                         "margin-right": "auto", "width": "33%"}),
-         html.Div(dcc.Dropdown(id="select-yaxis", options=[{'label': i.title(), 'value': i} for i in df.columns[3:]],
-                               value='pork', ), className="four columns",
-                  style={"display": "block", "margin-left": "auto",
-                         "margin-right": "auto", "width": "33%"}),
-         html.Div(dcc.Dropdown(id="select-zaxis", options=[{'label': i.title(), 'value': i} for i in df.columns[3:]],
-                               value='poultry', ), className="four columns",
-                  style={"display": "block", "margin-left": "auto",
-                         "margin-right": "auto", "width": "33%"})
+        [html.Div(dcc.Dropdown(id="select-satellite", options=[{'label': 'Satellite #%s' % i, 'value': i}
+                                                               for i in df.sat_id.value_counts().index.sort_values()],
+                               value=[0], multi=True), className="first-column",
+                  style={"display": "block", "margin-left": "auto", "margin-right": "auto", "width": "33%"}),
          ], className="row", style={"padding": 14, "display": "block", "margin-left": "auto",
                                     "margin-right": "auto", "width": "80%"}),
     html.Div([dcc.Graph(id="my-graph")])
@@ -39,21 +32,17 @@ layout = html.Div([
 
 @app.callback(
     dash.dependencies.Output("my-graph", "figure"),
-    [dash.dependencies.Input("select-xaxis", "value"),
-     dash.dependencies.Input("select-yaxis", "value"),
-     dash.dependencies.Input("select-zaxis", "value")]
+    [dash.dependencies.Input("select-satellite", "value")]
 )
-def ugdate_figure(selected_x, selected_y, selected_z):
-    z = df[selected_z]
+def ugdate_figure(selected_satellites):
+    udf = df[df.sat_id.isin(selected_satellites)]
     trace = [go.Scatter3d(
-        x=df[selected_x], y=df[selected_y], z=df[selected_z],
-        mode='markers', marker={'size': 8, 'color': z, 'colorscale': 'Blackbody', 'opacity': 0.8, "showscale": True,
-                                "colorbar": {"thickness": 15, "len": 0.5, "x": 0.8, "y": 0.6, }, })]
+        x=udf[udf.sat_id == i].x, y=udf[udf.sat_id == i].y, z=udf[udf.sat_id == i].z,
+        mode='markers', marker={'size': 8, 'colorscale': 'Blackbody', 'opacity': 0.8 })
+        for i in udf.sat_id.value_counts().index.sort_values()]
     return {"data": trace,
             "layout": go.Layout(
-                height=700, title=f"Exports<br>{selected_x.title(), selected_y.title(), selected_z.title()}",
+                height=700, title=f"Satellites <br>{selected_satellites}",
                 paper_bgcolor="#f3f3f3",
-                scene={"aspectmode": "cube", "xaxis": {"title": f"{selected_x.title()} (USD)", },
-                       "yaxis": {"title": f"{selected_y.title()} (USD)", },
-                       "zaxis": {"title": f"{selected_z.title()} (USD)", }})
+                scene={"aspectmode": "cube", })
             }
